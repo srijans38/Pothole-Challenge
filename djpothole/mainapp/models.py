2dbdbd4bbced1b89434fe653604b4ee236471452
SohailTSM <sohailmemon10@gmail.com>
2020-01-05 12:04:58 +0530

    added password change form

--- djpothole/mainapp/models.py
+++ djpothole/mainapp/models.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 
 
 class UData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='u')
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='u')
     resign_date = models.DateTimeField(null=True, blank=True)
 
     region = models.CharField(max_length=128)
+    ever_logged_in = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.user)
