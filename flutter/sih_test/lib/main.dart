25c8ea797573322855f2c2cca409752d1c894f4d
Srijan Sharma <srijans38@gmail.com>
2020-02-29 16:11:47 +0530

    test

--- flutter/sih_test/lib/main.dart
+++ flutter/sih_test/lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sih_test/screens/login_screen.dart';
+import 'package:sih_test/screens/main_screen.dart';
 import 'package:sih_test/services/auth_widget.dart';
 import 'package:sih_test/services/firebase_auth_service.dart';
 import 'package:sih_test/services/firebase_storage_service.dart';
